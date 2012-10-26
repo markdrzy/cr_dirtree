2067a5e80a5b09cd358f66535953388f3da10bd1
Mark Drzycimski <mark@clickrain.com>
2012-10-25 21:53:15 -0400

    Added a little boilerplate.

---
+++ pi.cr_dirtree.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine DirTree Plugin
+ *
+ * @package    	DirTree Plugin
+ * @subpackage	Plugins
+ * @category	Plugins
+ * @author		Mark Drzycimski
+ * @link		http://https://github.com/mark-cr
+ */
+
+public function usage()
+{
+    
+}
+
+
+?>
