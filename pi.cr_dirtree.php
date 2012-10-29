b14d679fdf2011d5d910df42e768e50dabd911fb
Mark Drzycimski <mark@clickrain.com>
2012-10-28 19:22:36 -0500

    Documentation Updates

--- pi.cr_dirtree.php
+++ pi.cr_dirtree.php
@@ -218,23 +218,23 @@ This plugin will return an HTML-formatted ordered list of all files and folders 
 
 ### fud_id ###
 
-The numeric id representing the file upload destination you wish to view (Example: 4). It is __required__ that you provide either an _fud_id_ or an _fud_name_.
+The numeric id representing the file upload destination you wish to view (Example: 4). It is __required__ that you provide either a <code>fud_id</code> or a <code>fud_name</code>.
 
 ### fud_name ###
 
-The name given for the file upload destination as found in the file upload preferences (Example: General File Uploads). It is __required__ that you provide either an _fud_id_ or an _fud_name_.
+The name given for the file upload destination as found in the file upload preferences (Example: General File Uploads). It is __required__ that you provide either a <code>fud_id</code> or a <code>fud_name</code>.
 
 ### base_list_id ###
 
-You may specify an id for the base <pre><ol></pre> using the _base_list_id_ parameter (Example: fileList). _Default value: none_
+You may specify an id for the base <code>&lt;ol&gt;</code> using the <code>base_list_id</code> parameter (Example: fileList). _Default value: none_
 
 ### base_list_class ###
 
-You may specify a class for the base <pre><ol></pre> using the _base_list_class_ parameter (Example: fileList). _Default value: none_
+You may specify a class for the base <code>&lt;ol&gt;</code> using the <code>base_list_class</code> parameter (Example: fileList). _Default value: none_
 
 ### site_id ###
 
-You may specify a _site_id_ if you are utilizing the Multi-site Manager (MSM) (Example: 3). _Default value: 1_
+You may specify a <code>site_id</code> if you are utilizing the Multi-site Manager (MSM) (Example: 3). _Default value: 1_
 
 	<?php
 		$buffer = ob_get_contents();
